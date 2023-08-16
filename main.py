d38af04a37f7b39dfe74cffda6c31a4a656f1554
Felipe Sena <64341375+fsenx@users.noreply.github.com>
2023-08-15 21:49:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from test import *
 from trata_dados import *
 
-def run(request=None)
+def run(request=None):
   df = pd.read_csv('dados.csv', sep=',')
   print(len(df))
   return 200
